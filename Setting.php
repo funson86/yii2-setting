f9f7c099c4ae75093d13a768d7437413417fb216
funson86 <funson86@gmail.com>
2015-01-26 16:57:29 +0800

    bug fix

--- Setting.php
+++ Setting.php
@@ -2,15 +2,14 @@
 
 namespace funson86\setting;
 use Yii;
-use funson86\setting\models\Setting;
 
-class Settings extends \yii\base\Component
+class Setting extends \yii\base\Component
 {
     public function get($code)
     {
         if(!$code) return ;
 
-        $setting = Setting::find()->where(['code' => $code])->one();
+        $setting = \funson86\setting\models\Setting::find()->where(['code' => $code])->one();
 
         if($setting)
             return $setting->value;
