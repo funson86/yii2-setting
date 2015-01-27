54a771ed7cdb8133a00966e3721f6bd003ff5997
funson86 <funson86@gmail.com>
2015-01-27 10:00:18 +0800

    code style

--- views/default/index.php
+++ views/default/index.php
@@ -9,24 +9,22 @@ $this->title = Module::t('setting', 'Setting');
 $this->params['breadcrumbs'][] = $this->title;
 
 $items = [];
-foreach($settingParent as $parent)
-{
+foreach ($settingParent as $parent) {
     $item['label'] = Module::t('setting', $parent->code);
 
     $str = '';
     $children = Setting::find()->where(['parent_id' => $parent->id])->orderBy(['sort_order' => SORT_ASC, 'id' => SORT_ASC])->all();
-    foreach($children as $child)
-    {
+    foreach ($children as $child) {
         $str .= '<div class="form-group field-blogcatalog-parent_id"><label class="col-lg-2 control-label" for="blogcatalog-parent_id">' . Module::t('setting', $child->code) . '</label><div class="col-lg-3">';
 
-        if($child->type == 'text')
+        if ($child->type == 'text')
             $str .= Html::textInput("Setting[$child->code]", $child->value, ["class" => "form-control"]);
-        elseif($child->type == 'password')
+        elseif ($child->type == 'password')
             $str .= Html::passwordInput("Setting[$child->code]", $child->value, ["class" => "form-control"]);
-        elseif($child->type == 'select') {
+        elseif ($child->type == 'select') {
             $options = [];
             $arrayOptions = explode(',', $child->store_range);
-            foreach($arrayOptions as $option)
+            foreach ($arrayOptions as $option)
                 $options[$option] = Module::t('setting', $option);
 
             $str .= Html::dropDownList("Setting[$child->code]", $child->value, $options, ["class" => "form-control"]);
